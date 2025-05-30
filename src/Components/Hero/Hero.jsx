a9ac357079183beb37c78b671ffffb91a7679ffe
Gauravvgithub <gauravraj7011@gmail.com>
2025-05-30 21:52:26 +0530

    cv update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import resume_gaurav from "../../assets/GAURAV_RESUME_2025.pdf";
+import resume_gaurav from "../../assets/GAURAV-RESUME.pdf";
 
 const Hero = () => {
   return (
